8ec6963a4058ba8b83f6abe1b43a24f25738be64
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2025-03-21 15:39:51 +0400

    Add modal opening logic to the header file

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,27 +1,42 @@
 import Momentum from "../assets/Momentum.svg";
 import Hourglass from "../assets/Hourglass.png";
 import Add from "../assets/Add.svg";
+import { useState } from "react";
+import Modal from "../components/Modal.tsx";
 
 const Header = () => {
-  return (
-    <div className="fixed top-0 left-0 z-50 w-full bg-white px-[120px] py-[15px]">
-      <div className="flex items-center justify-between py-[30px]">
-        <div className="flex">
-          <img src={Momentum} alt="logo" />
-          <img src={Hourglass} alt="hourglass" className="h-[38px] w-[38px]" />
-        </div>
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-        <div className="flex gap-10 text-[16px]">
-          <button className="cursor-pointer rounded-[5px] border-[1px] border-[#8338EC] px-[20px] py-2.5 hover:border-[#B588F4] hover:duration-500">
-            თანამშრომლის შექმნა
-          </button>
-          <button className="flex items-center justify-center gap-0.5 rounded-[5px] border bg-[#8338EC] px-[20px] py-2.5 text-nowrap text-white hover:bg-[#B588F4] hover:duration-500">
-            <img src={Add} alt="add-button" className="h-5 w-5" />
-            <p>შექმენი ახალი დავალება</p>
-          </button>
+  return (
+    <>
+      <div className="fixed top-0 left-0 z-50 w-full bg-white px-[120px] py-[15px]">
+        <div className="flex items-center justify-between py-[30px]">
+          <div className="flex">
+            <img src={Momentum} alt="logo" />
+            <img
+              src={Hourglass}
+              alt="hourglass"
+              className="h-[38px] w-[38px]"
+            />
+          </div>
+
+          <div className="flex gap-10 text-[16px]">
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="cursor-pointer rounded-[5px] border-[1px] border-[#8338EC] px-[20px] py-2.5 hover:border-[#B588F4] hover:duration-500"
+            >
+              თანამშრომლის შექმნა
+            </button>
+            <button className="flex cursor-pointer items-center justify-center gap-0.5 rounded-[5px] border bg-[#8338EC] px-[20px] py-2.5 text-nowrap text-white hover:bg-[#B588F4] hover:duration-500">
+              <img src={Add} alt="add-button" className="h-5 w-5" />
+              <p>შექმენი ახალი დავალება</p>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </>
   );
 };
 
